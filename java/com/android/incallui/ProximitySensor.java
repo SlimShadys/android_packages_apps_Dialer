5360336a12de5f650054f3a55a21db4eaebb0558
DennySPB <dennyspb@gmail.com>
2019-03-04 10:25:28 +0300

    Fix incorrect proximity sensor behaviour
    * screen must be ON when audioroute != EARPICE
    
    Change-Id: Iaaf9a4ffb91024a92f06494965be17be85322d39
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- java/com/android/incallui/ProximitySensor.java
+++ java/com/android/incallui/ProximitySensor.java
@@ -190,6 +190,7 @@ public class ProximitySensor
 
     if (hasOngoingCall && InCallState.OUTGOING == oldState) {
       setProxSpeaker(isProxSensorFar);
+      updateProximitySensorMode();
     }
 
     if (hasIncomingCall) {
